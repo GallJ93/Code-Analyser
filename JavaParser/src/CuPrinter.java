d586fedd0bc1fdaea0679c1eb599307cf27e1f3f
Andrew Conway <andrewconway92@googlemail.com>
2013-11-29 10:20:57 +0000

    Initial JavaParser code.

---
+++ JavaParser/src/CuPrinter.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+
+import japa.parser.JavaParser;
+import japa.parser.ast.CompilationUnit;
+
+public class CuPrinter {
+
+    public static void main(String[] args) throws Exception {
+        // creates an input stream for the file to be parsed
+        FileInputStream in = new FileInputStream("/Users/andrewconway/Documents/workspace/BouncingBall/src/mvc/BouncingBall.java");
+
+        CompilationUnit cu;
+        try {
+            // parse the file
+            cu = JavaParser.parse(in);
+        } finally {
+            in.close();
+        }
+
+        // prints the resulting compilation unit to default system output
+        System.out.println(cu.toString());
+    }
+}

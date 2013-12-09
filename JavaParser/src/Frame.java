151e1fba58f9b97eca74f12116b11c32ee6f1bc0
Andrew Conway <andrewconway92@googlemail.com>
2013-12-09 01:55:23 +0000

    Frame

---
+++ JavaParser/src/Frame.java
@@ -0,0 +1,51 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTable;
+
+
+public class Frame extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frame frame = new Frame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 654, 510);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		DefaultTableModel model = new DefaultTableModel(); 
+		JTable table = new JTable(model); 
+
+		// Create a couple of columns 
+		model.addColumn("Col1"); 
+		model.addColumn("Col2"); 
+	}
+
+}
